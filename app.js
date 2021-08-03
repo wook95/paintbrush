696908e98e85835fdd9839b5a588f7cfb9da882b
wook95 <uclee95@gmail.com>
2021-08-03 18:03:33 +0900

    Implement color changing, bursh size modification

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
-
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
 
 canvas.width = 500;
 canvas.height = 700;
@@ -33,7 +34,15 @@ function onMouseMove(event) {
   }
 }
 
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
 
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
 
 
 if (canvas) {
@@ -41,6 +50,15 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+}
+
+
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
+
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
 }
 
 
